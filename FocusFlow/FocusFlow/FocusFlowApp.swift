397f6636c312947c1856e6085feab4194b5dd9b3
kotyk141191 <mikl.kotyk@gmail.com>
2025-03-14 12:37:16 +0200

    init commit

---
+++ FocusFlow/FocusFlow/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  FocusFlow
+//
+//  Created by Mykhailo Kotyk on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
